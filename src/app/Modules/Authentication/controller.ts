bace52302f1a380b234aef64ea66b6d180cf0840
Lucas Barros <lbarros.comp@gmail.com>
2021-06-10 18:10:03 -0300

    Inserting a example try catch to login

--- src/app/Modules/Authentication/controller.ts
+++ src/app/Modules/Authentication/controller.ts
@@ -3,23 +3,27 @@ import { getUserByEmail } from '../Users/core';
 import { validateAccess } from './core';
 
 export const login = async (request: { body: ILogin }, response: any) => {
-  const { email, password } = request.body;
-  if (!email || !password) {
-      return response.status(400).send({
-        error:
-          'Missing e-mail or password data. Please fill all the form fields and try again or contact support.',
-      });
-  }
+  try {
+    const { email, password } = request.body;
+    if (!email || !password) {
+        return response.status(400).send({
+          error:
+            'Missing e-mail or password data. Please fill all the form fields and try again or contact support.',
+        });
+    }
 
-  const user = await getUserByEmail(email);
-  if (!user) {
-    return response.status(401).send({ message: 'User not registered' });
-  }
+    const user = await getUserByEmail(email);
+    if (!user) {
+      return response.status(401).send({ message: 'User not registered' });
+    }
 
-  const validatedAccess = await validateAccess({ email, password });
-  if (!validatedAccess) {
-    return response.status(401).send({ message: 'Email or password incorrect' });
-  }
+    const validatedAccess = await validateAccess({ email, password });
+    if (!validatedAccess) {
+      return response.status(401).send({ message: 'Email or password incorrect' });
+    }
 
-  return response.send(validatedAccess);
+    return response.send(validatedAccess);
+  } catch(err) {
+    return response.status(500).send({ message: 'Error to login' });
+  }
 };
